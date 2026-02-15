b41a2b705ed6f7f0ffc88453a061999109b61b2a
mashijifu <4ts0t0i7c@gmail.com>
2026-02-16 08:22:39 +0900

    投稿機能の基本実装（PostgreSQL、モデル・コントローラ・ビュー・スタイル）。Gemfile の PostgreSQL 対応と日本語化を追加。

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: I18n.t("views.posts.created")
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: I18n.t("views.posts.updated")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: I18n.t("views.posts.destroyed")
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
